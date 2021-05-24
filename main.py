0f57595c0f413e63e6f1267b0d625e1431433bb0
Enqie <notastrict@gmail.com>
2021-05-24 12:53:54 +1000

    0.3.2-a

--- main.py
+++ main.py
@@ -13,33 +13,38 @@ defaultdata = {
 "crops": {}
 }
 
+ver = '0.3.2-a'
 pdata = None
+
 try:
   with open('pdata.json','r') as f:
     pdata = json.load(f)
   print(f'''
-Welcome back {pdata['name']}
-  ''')
+antfrm {ver}
+Welcome back {pdata['name']}''')
 except FileNotFoundError:
     pdata = defaultdata
-    pdata['name'] = input("Welcome to antfrm\n Input Name\n> ")
+    pdata['name'] = input(f'''antfrm {ver}
+Input Name
+
+> ''')
     with open('pdata.json', 'w') as f:
       json.dump(pdata, f)
   
 def help(args):
   print(f'''Commands:
-| h - Shows this menu
-| x - Exit game
-| p - Shows your profile
-| f - Harvest crops
-| b - Buy farms (b [crop] [amount])
-| m - Opens shop menu (WIP)
+  h - Shows this menu
+  x - Exit game
+  p - Shows your profile
+  f - Harvest crops
+  b - Buy farms (b [crop] [amount])
+  s - Sells all crops
+  m - Opens shop menu (WIP)
 
 Options:
-|~n - Change your name
-|~a - Toggle Autosell
-|~r - Reset game
-  ''')
+ ~n - Change your name
+ ~a - Toggle Autosell
+ ~r - Reset game''')
 
 def quit(args):
   print("Goodbye\n")
@@ -49,29 +54,36 @@ def stats(args):
   with open('pdata.json','r') as f:
     pdata = json.load(f)
   print(f'''{pdata['name']}'s Profile:
-| Money: {pdata['money']}
-| Multiplier: {pdata['multiplier']}x
-| Farm Slots: {pdata['farmslots']}
-''')
+  Money: {pdata['money']}
+  Multiplier: {pdata['multiplier']}x
+  Farm Slots: {pdata['farmslots']}''')
 
 def namechange(args):
-  newn = input("New Name: (Leave blank for no change)\n~")
+  newn = input(f'''New Name: (Leave blank for no change)
+  
+~ ''')
   if newn != '':
     pdata['name'] = newn
     with open('pdata.json', 'w') as f:
       json.dump(pdata, f)
+    print('')
   else:
-    print('Name not changed\n')
+    print('Name not changed')
   
 def reset(args):
-  ans = input("Are you sure you want to reset? (y/N)\n~ ")
+  ans = input(f'''Are you sure you want to reset? (y/N)
+  
+~ ''')
   if ans.lower() == "y":
     pdata = defaultdata
-    pdata['name'] = input("Welcome to antfrm\n Input Name\n> ")
+    pdata['name'] = input(f'''antfrm {ver}
+Input Name
+
+> ''')
     with open('pdata.json', 'w') as f:
       json.dump(pdata, f)
   else:
-    print("Reset cancelled\n")
+    print("Reset cancelled")
 
 def buy(args):
   with open('pdata.json','r') as f:
@@ -85,7 +97,7 @@ def buy(args):
   else:
     clist = len(clist)
   if args == []:
-    print('Type "s" for shop\n')
+    print('Type "s" for shop')
   elif args[0] in pdata['farms']:
     try:
       amt = int(args[1])
@@ -97,48 +109,56 @@ def buy(args):
         pdata['money'] -= crops[args[0]]['buy-price']
         with open('pdata.json', 'w') as f:
           json.dump(pdata, f)
-        print(f'''Bought {amt}x {args[0]} farm (-${crops[args[0]]['buy-price'] * amt})
-        ''')
+        print(f'''Bought {amt}x {args[0]} farm (-${crops[args[0]]['buy-price'] * amt})''')
       else:
         print('No available farm slots.\n')
     else:
-      print(f'''Not enough money, you need ${crops[args[0]]['buy-price']*amt - pdata['money']} more.
-      ''')
+      print(f'''Not enough money, you need ${crops[args[0]]['buy-price']*amt - pdata['money']} more.''')
   else:
-    print('Type "s" for shop\n')
+    print('Type "s" for shop')
+
+def sell(args):
+  if pdata['crops'].values() != 0:
+    for crop, count in pdata['crops'].items():
+      sellamt += pdata['crops'][crop] * crops[crop]['sell-price']
+      print(sellamt)
+    pdata['money'] = pdata['money'] + sellamt
+    print(f'''Sold all for ${sellamt}''')
+  else:
+    print('a')
+  with open('pdata.json', 'w') as f:
+    json.dump(pdata, f)
 
 def shop(args):
   with open('pdata.json','r') as f:
     pdata = json.load(f)
-  print("to do: market\n")
+  print("to do: market")
 
 def harvest(args):
   with open('pdata.json','r') as f:
     pdata = json.load(f)
   if sum(pdata['farms'].values()) == 0:
-    print("You have no farms. Buy some from the market\n")
+    print("You have no farms. Buy some from the market")
   else:
     totals = {}
     sellamt = 0
     print("Your ants collected:")
     for farm, count in pdata['farms'].items():
-      totals[farm] = int(round(count * crops[farm]['yield'] * random.uniform(1,2.5),0))
+      totals[farm] = int(round(count * crops[farm]['yield'] * random.uniform(1,2.5),0)) #yield
       try:
         pdata['crops'][farm] += totals[farm]
       except:
         pdata['crops'] = pdata['crops'] | totals
-      with open('pdata.json', 'w') as f:
-        json.dump(pdata, f)
       if totals[farm] > 0:
-        print(f'''| + {totals[farm]} {farm}''')
+        print(f''' +{totals[farm]} {farm}''')
       if pdata['autosell'] == "true":
-        for crop, count in pdata["crops"].items():
-          sellamt += count * crops[crop]['sell-price']
-          pdata['crops'][crop] -= count
-          pdata['money'] += sellamt
+        sellamt += totals[farm] * crops[farm]['sell-price']
+        pdata['crops'][farm] -= count
+        pdata['money'] += sellamt
+    with open('pdata.json', 'w') as f:
+      json.dump(pdata, f)
     if sellamt != 0:
-      print(f'''| Sold all for ${sellamt}
-      ''')
+      print(f''' Sold all for ${sellamt}''')
     else:
       print("")
     
@@ -147,12 +167,12 @@ def togglesell(args):
     pdata = json.load(f)
   if pdata['autosell'] == "true":
     pdata['autosell'] = "false"
-    print("| Autosell toggled off\n")
+    print("Autosell toggled off")
     with open('pdata.json', 'w') as f:
       json.dump(pdata, f)
   elif pdata['autosell'] == "false":
     pdata['autosell'] = "true"
-    print("| Autosell toggled on\n")
+    print("Autosell toggled on")
     with open('pdata.json', 'w') as f:
       json.dump(pdata, f)
     
@@ -165,6 +185,7 @@ commands = {
   'p': stats,
   'f': harvest,
   'b': buy,
+  's': sell,
   'm': shop,
   '~n': namechange,
   '~r': reset,
@@ -172,5 +193,5 @@ commands = {
 }
 
 while True:
-  inp = input('> ').split(' ')
+  inp = input('\n> ').split(' ')
   commands.get(inp[0], invalid_command)(inp[1:])
